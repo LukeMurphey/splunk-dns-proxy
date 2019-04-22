a4ce05ed7d6bb007f9250020c95fd287a0bedf8c
LukeMurphey <Luke@LukeMurphey.net>
2019-04-22 16:59:30 -0400

    Improving logging

--- src/bin/dns_proxy.py
+++ src/bin/dns_proxy.py
@@ -1,12 +1,15 @@
+"""
+This module provides a Splunk Modular input that operates a DNS proxy. The DNS
+proxy logs requests and replies in Splunk.
+"""
 import sys
 import os
-import time
 import binascii
 
 path_to_mod_input_lib = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'modular_input.zip')
 sys.path.insert(0, path_to_mod_input_lib)
 
-from modular_input import IntegerField, Field, ModularInput
+from modular_input import IntegerField, DomainNameField, Field, ModularInput
 
 import splunk.appserver.mrsparkle.lib.util as util
 lib_dir = os.path.join(util.get_apps_dir(), 'dns_proxy', 'bin', 'dns_proxy_app')
@@ -18,17 +21,12 @@ from event_writer import StashNewWriter
 
 from dnslib.proxy import ProxyResolver, PassthroughDNSHandler
 from dnslib.server import DNSLogger, DNSServer
-from dnslib import DNSRecord,DNSError,QTYPE,RCODE,RR
+from dnslib import DNSRecord, DNSError, QTYPE, RCODE, RR
 
 class SplunkDNSLogger:
     def __init__(self, index, source, sourcetype):
         self.writer = StashNewWriter(index=index, source_name=source, sourcetype=sourcetype,
                                 file_extension=".stash_output")
-
-    def print_message(self, message):
-        self.writer.write_event({
-            'message': message,
-        })
 
     def log_prefix(self,handler):
         return ""
@@ -37,31 +35,34 @@ class SplunkDNSLogger:
         pass
 
     def log_recv(self,handler,data):
-        self.print_message("%sReceived: [%s:%d] (%s) <%d> : %s" % (
-                    self.log_prefix(handler),
-                    handler.client_address[0],
-                    handler.client_address[1],
-                    handler.protocol,
-                    len(data),
-                    binascii.hexlify(data)))
+        self.writer.write_event({
+            'type': 'received',
+            'client_address': handler.client_address[0],
+            'client_port': handler.client_address[1],
+            'protocol': handler.protocol,
+            'length': len(data),
+            'data': binascii.hexlify(data)
+        })
 
     def log_send(self,handler,data):
-        self.print_message("%sSent: [%s:%d] (%s) <%d> : %s" % (
-                    self.log_prefix(handler),
-                    handler.client_address[0],
-                    handler.client_address[1],
-                    handler.protocol,
-                    len(data),
-                    binascii.hexlify(data)))
+        self.writer.write_event({
+            'type': 'sent',
+            'client_address': handler.client_address[0],
+            'client_port': handler.client_address[1],
+            'protocol': handler.protocol,
+            'length': len(data),
+            'data': binascii.hexlify(data)
+        })
 
     def log_request(self,handler,request):
-        self.print_message("%sRequest: [%s:%d] (%s) / '%s' (%s)" % (
-                    self.log_prefix(handler),
-                    handler.client_address[0],
-                    handler.client_address[1],
-                    handler.protocol,
-                    request.q.qname,
-                    QTYPE[request.q.qtype]))
+        self.writer.write_event({
+            'type': 'request',
+            'client_address': handler.client_address[0],
+            'client_port': handler.client_address[1],
+            'protocol': handler.protocol,
+            'query_name': request.q.qname,
+            'response_type': QTYPE[request.q.qtype]
+        })
         self.log_data(request)
 
     def log_reply(self,handler,reply):
@@ -76,57 +77,64 @@ class SplunkDNSLogger:
                 'response_records': ",".join([QTYPE[a.rtype] for a in reply.rr])
             })
         else:
-            self.print_message("%sReply: [%s:%d] (%s) / '%s' (%s) / %s" % (
-                    self.log_prefix(handler),
-                    handler.client_address[0],
-                    handler.client_address[1],
-                    handler.protocol,
-                    reply.q.qname,
-                    QTYPE[reply.q.qtype],
-                    RCODE[reply.header.rcode]))
+            self.writer.write_event({
+                'type': 'reply',
+                'client_address': handler.client_address[0],
+                'client_port': handler.client_address[1],
+                'protocol': handler.protocol,
+                'query_name': reply.q.qname,
+                'response_type': QTYPE[reply.q.qtype],
+                'response_code': reply.header.rcode
+            })
         self.log_data(reply)
 
     def log_truncated(self,handler,reply):
-        self.print_message("%sTruncated Reply: [%s:%d] (%s) / '%s' (%s) / RRs: %s" % (
-                    self.log_prefix(handler),
-                    handler.client_address[0],
-                    handler.client_address[1],
-                    handler.protocol,
-                    reply.q.qname,
-                    QTYPE[reply.q.qtype],
-                    ",".join([QTYPE[a.rtype] for a in reply.rr])))
-        self.log_data(reply)
+        self.writer.write_event({
+            'type': 'truncated_reply',
+            'client_address': handler.client_address[0],
+            'client_port': handler.client_address[1],
+            'protocol': handler.protocol,
+            'query_name': reply.q.qname,
+            'response_type': QTYPE[reply.q.qtype],
+            'response_records': ",".join([QTYPE[a.rtype] for a in reply.rr])
+        })
 
     def log_error(self,handler,e):
-        self.print_message("%sInvalid Request: [%s:%d] (%s) :: %s" % (
-                    self.log_prefix(handler),
-                    handler.client_address[0],
-                    handler.client_address[1],
-                    handler.protocol,
-                    e))
+        self.writer.write_event({
+            'type': 'invalid_request',
+            'client_address': handler.client_address[0],
+            'client_port': handler.client_address[1],
+            'protocol': handler.protocol,
+            'error': str(e)
+        })
 
     def log_data(self,dnsobj):
         pass
         #print("\n", dnsobj.toZone("    "), "\n", sep="")
 
 class DNSProxyInput(ModularInput):
+    """
+    The main class that executes the modular input.
+    """
     def __init__(self):
-
-        scheme_args = {'title': "DNS Proxy Server",
-                        'description': "Provides a DNS server for the purposes of monitoring DNS requests.",
-                        'use_external_validation': "true",
-                        'streaming_mode': "xml",
-                        'use_single_instance': "true"}
+        scheme_args = {
+            'title': "DNS Proxy Server",
+            'description': "Provides a DNS server for the purposes of monitoring DNS requests.",
+            'use_external_validation': "true",
+            'streaming_mode': "xml",
+            'use_single_instance': "true"
+        }
 
         args = [
-                IntegerField("port", "Port", "The port to server the proxy server on", empty_allowed=False),
-                Field("upstream_dns", "Upstream DNS Server", "The DNS server to pass the DNS queries to (e.g. 192.168.0.1:53)", empty_allowed=False),
-                Field("address", "Address", "The address to serve the DNS proxy on", empty_allowed=True)
+            Field("upstream_dns", "Upstream DNS Server", "The DNS server to pass the DNS queries to (e.g. 208.67.222.222)", empty_allowed=False),
+            DomainNameField("address", "Address", "The address to serve the DNS proxy on; leave blank to operate the DNS server on all interfaces", empty_allowed=True),
+            IntegerField("port", "Port", "The port to server the proxy server on", empty_allowed=False)
         ]
 
         ModularInput.__init__(self, scheme_args, args, logger_name='dns_proxy_modular_input')
 
         self.udp_server = None
+        self.tcp_server = None
 
     def run(self, stanza, cleaned_params, input_config):
         # Stop if the DNS server already exists
@@ -148,7 +156,7 @@ class DNSProxyInput(ModularInput):
         upstream_dns_port = int(upstream_dns_port or 53)
 
         resolver = ProxyResolver(upstream_dns_server, upstream_dns_port, 5)
-        handler = PassthroughDNSHandler # or ProxyResolver
+        handler = PassthroughDNSHandler
         dns_logger = SplunkDNSLogger(index, source, sourcetype)
 
         # Start the UDP server
